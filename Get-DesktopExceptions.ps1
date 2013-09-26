4afaa56681dacaaa6c4da6945e1ef2bf6a670b85
seanpmassey <seanpmassey@gmail.com>
2013-09-25 23:57:00 -0500

    Updated scripts
    Added context-based help and parameterized scripts

--- Get-DesktopExceptions.ps1
+++ Get-DesktopExceptions.ps1
@@ -1,29 +1,36 @@
 <#
 .SYNOPSIS
-   <A brief description of the script>
+   Get-DesktopExceptions is a script that will locate VMware View Linked Clones that are not using the correct snapshot/image.  The script also contains an option to remediate any non-compliant desktops by deleting them and letting View recreate them.
 .DESCRIPTION
-   <A detailed description of the script>
-.PARAMETER <paramName>
-   <Description of script parameter>
+   Get-DesktopExceptions will look in the View LDAP datastore to find the snapshot IDs used by the desktops and the pool. It compares these values to find any desktops that do not match the pool.  If the -Remediate switch is selected, the script will then remove them.  In order to run this script, the Quest Active Directory Cmdlets will need to be installed.
+.PARAMETER ConnectionServer
+   The View Connection server that you want to run this script against.
+.PARAMETER Remediate
+   Delete desktops that do not have the correct snapshots
+.PARAMETER EmailRcpt
+   Person or group who should receive the email report
+.PARAMETER SMTPServer
+   Email server
 .EXAMPLE
-   <An example of using the script>
+   Get-DesktopExceptions -ConnectionServer connection.domain.com -Remediate -EmailRcpt user@domain.com -SMTPServer smtp.domain.com
 #>
+param($ConnectionServer,[switch]$Remediate,$EmailRcpt,$SMTPServer)
 
 Function Send-Email
 {
- Param([string]$SMTPBody,[string]$SMTPSubject = "View Snapshot Compliance Report",[string]$SMTPTo)
-Send-MailMessage -To $SMTPTo -Body $SMTPBody -Subject $SMTPSubject -SmtpServer smtp.gbdioc.org -From "Notifications_noreply@gbdioc.org" -BodyAsHtml
+ Param([string]$SMTPBody,[string]$SMTPSubject = "View Snapshot Compliance Report",[string]$SMTPTo,$SMTPServer)
+Send-MailMessage -To $SMTPTo -Body $SMTPBody -Subject $SMTPSubject -SmtpServer $SMTPServer -From "Notifications_noreply@gbdioc.org" -BodyAsHtml
 }
 
 
 Function Get-Pools
 {
-param($ConnectionServer,[switch]$Remediate,$EmailRcpt)
+param($ConnectionServer)
 
 $PoolList = @()
 
 $arrIncludedProperties = "cn,name,pae-DisplayName,pae-MemberDN,pae-SVIVmParentVM,pae-SVIVmSnapshot,pae-SVIVmSnapshotMOID".Split(",")
-$pools = Get-QADObject -Service applvcs03.gbcdnt.com -DontUseDefaultIncludedProperties -IncludedProperties $arrIncludedProperties -LdapFilter "(objectClass=pae-ServerPool)" -SizeLimit 0 | Sort-Object "pae-DisplayName" | Select-Object Name, "pae-DisplayName", "pae-SVIVmParentVM" , "pae-SVIVmSnapshot", "pae-SVIVmSnapshotMOID", "pae-MemberDN"
+$pools = Get-QADObject -Service $ConnectionServer -DontUseDefaultIncludedProperties -IncludedProperties $arrIncludedProperties -LdapFilter "(objectClass=pae-ServerPool)" -SizeLimit 0 | Sort-Object "pae-DisplayName" | Select-Object Name, "pae-DisplayName", "pae-SVIVmParentVM" , "pae-SVIVmSnapshot", "pae-SVIVmSnapshotMOID", "pae-MemberDN"
 
 ForEach($pool in $pools)
 {
@@ -43,24 +50,24 @@ Return $PoolList
 
 Function Get-Desktop
 {
-param($MemberDN, $PoolSnapshotMOID)
+param($MemberDN, $ConnectionServer)
 
 $arrIncludedProperties = "cn,name,pae-DisplayName,pae-MemberDN,pae-SVIVmParentVM,pae-SVIVmSnapshot,pae-SVIVmSnapshotMOID".Split(",")
-$Desktop = Get-QADObject -Service applvcs03.gbcdnt.com -DontUseDefaultIncludedProperties -IncludedProperties $arrIncludedProperties -LdapFilter "(&(objectClass=pae-Server)(distinguishedName=$MemberDN))" -SizeLimit 0 | Sort-Object "pae-DisplayName" | Select-Object Name, "pae-DisplayName", "pae-SVIVmParentVM" , "pae-SVIVmSnapshot", "pae-SVIVmSnapshotMOID"
+$Desktop = Get-QADObject -Service $ConnectionServer -DontUseDefaultIncludedProperties -IncludedProperties $arrIncludedProperties -LdapFilter "(&(objectClass=pae-Server)(distinguishedName=$MemberDN))" -SizeLimit 0 | Sort-Object "pae-DisplayName" | Select-Object Name, "pae-DisplayName", "pae-SVIVmParentVM" , "pae-SVIVmSnapshot", "pae-SVIVmSnapshotMOID"
 
 Return $Desktop
 }
 
 
 $DesktopExceptions = @()
-$pools = Get-Pools
+$pools = Get-Pools -ConnectionServer $ConnectionServer
 
 ForEach($pool in $pools)
 {
 $MemberDNs = $pool.memberdn
 	ForEach($MemberDN in $MemberDNs)
 	{
-	$Desktop = Get-Desktop -MemberDN $MemberDN
+	$Desktop = Get-Desktop -MemberDN $MemberDN -ConnectionServer $ConnectionServer
 	
 	If($Desktop."pae-SVIVmSnapshotMOID" -ne $pool.SVIVmSnapshotMOID)
 		{
@@ -88,13 +95,13 @@ Else
 	$SMTPBody = $DesktopExceptions | Select-Object DisplayName,PoolName,PoolSnapshot,DesktopSVIVmSnapshot | ConvertTo-HTML
 	}
 
-Send-Email -SMTPBody $SMTPBody -SMTPTo "smassey@gbdioc.org"
+Send-Email -SMTPBody $SMTPBody -SMTPTo $EmailRcpt
 
 If($Remediate -eq $true)
 {
 	ForEach($Exception in $DesktopExceptions)
 	{
-		Set-QADObject -Identity $Exception.DesktopDN -Service applvcs03.gbcdnt.com -IncludedProperties "pae-vmstate" -ObjectAttributes @{"pae-vmstate"="DELETING"}
+		Set-QADObject -Identity $Exception.DesktopDN -Service $ConnectionServer -IncludedProperties "pae-vmstate" -ObjectAttributes @{"pae-vmstate"="DELETING"}
 	}
 	
 }
